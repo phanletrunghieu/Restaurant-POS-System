c2543fb6c23be2877eab5da470b291f1d0e97deb
Hieu Phan <hieutrunglephan@gmail.com>
2018-09-19 19:21:12 +0700

    declare model for entity framework & auto migrate

---
+++ DAL/Employee.cs
@@ -0,0 +1,35 @@
+namespace DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Employee")]
+    public partial class Employee
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Employee()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        [StringLength(1)]
+        public string Username { get; set; }
+
+        [StringLength(1)]
+        public string Password { get; set; }
+
+        public int? DepartmentID { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ DAL/Table.cs
@@ -0,0 +1,30 @@
+namespace DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Table")]
+    public partial class Table
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Table()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int? AreaID { get; set; }
+
+        public virtual Area Area { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
