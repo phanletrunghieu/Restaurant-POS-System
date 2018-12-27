600440b9e55b83cfc3eedd6327c667ec4b36ff6c
lehoangthienan <lehoangthienan@gmail.com>
2018-12-27 21:32:25 +0700

    Finish test CRUD area

--- UnitTest/UnitTestArea.cs
+++ UnitTest/UnitTestArea.cs
@@ -10,13 +10,40 @@ namespace UnitTest
     [TestClass]
     public class UnitTestArea
     {
+        private AreaBLL areaBLL = new AreaBLL();
+
         [TestMethod]
-        public void TestAddArea()
+        public void TestCRUDArea()
         {
-            string name = "BanVIP";
-            AreaBLL areaBLL = new AreaBLL();
-            Area area = areaBLL.CreateArea(name);
-            Assert.AreEqual(name, area.Name);
+            string nameExpected = "BanVIP";
+            Area area = areaBLL.CreateArea(nameExpected);
+            Assert.AreEqual(nameExpected, area.Name);
+            TestUpdateArea(area);
+        }
+        public void TestUpdateArea(Area area)
+        {
+            string nameTest = "BanThuong";
+            area.Name = nameTest;
+            areaBLL.Update(area);
+            List<Area> areas = areaBLL.ListArea();
+            string nameExpected= "";
+            for (int i=0; i < areas.Count; i++)
+            {
+                if (areas[i].ID == area.ID) nameExpected = areas[i].Name;
+            }
+            Assert.AreEqual(nameTest, nameExpected);
+            TestDeleteArea(area);
+        }
+        public void TestDeleteArea(Area area)
+        {
+            bool isDelete = true;
+            areaBLL.Delete(area);
+            List<Area> areas = areaBLL.ListArea();
+            for (int i = 0; i < areas.Count; i++)
+            {
+                if (areas[i].ID == area.ID) isDelete = false;
+            }
+            Assert.AreEqual(isDelete, true);
         }
     }
 }
