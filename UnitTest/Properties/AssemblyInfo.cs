bf7bfb84205f1d4d3003509c7a4b2aafb76e9060
lehoangthienan <lehoangthienan@gmail.com>
2018-12-27 19:16:12 +0700

    Add new unittest project, Add class demo to test create add areatet, add app.config and library of entity to test on database

--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,10 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("046030d2-018f-4e15-9583-b8e031373cff")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4beb606-bf1b-45d9-b0cf-96af971a1551")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTestArea.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using BLL;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestArea
+    {
+        [TestMethod]
+        public void TestAddArea()
+        {
+            string name = "BanVIP";
+            AreaBLL areaBLL = new AreaBLL();
+            Area area = areaBLL.CreateArea(name);
+            Assert.AreEqual(name, area.Name);
+        }
+    }
+}
