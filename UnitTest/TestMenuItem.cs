e55abb2d7b8dc59d7a0273212828aeaefca28432
lehoangthienan <lehoangthienan@gmail.com>
2018-12-29 01:36:09 +0700

    Add logic of unit test for test delete all menuitem and menu

--- UnitTest/TestMenuItem.cs
+++ UnitTest/TestMenuItem.cs
@@ -134,5 +134,57 @@ namespace UnitTest
                 Assert.AreNotEqual("", e.ToString());
             }
         }
+        [TestMethod]
+        public void TestDeleteAllMenuItemAndMenu()
+        {
+            string nameExpected = "Ăn rạng sáng";
+            int menuID=0;
+            Menu menu = menuBLL.CreateMenu(nameExpected);
+            List<Menu> menus = menuBLL.ListMenu();
+            for (int i = 0; i < menus.Count; i++)
+            {
+                if (menus[i].Name == nameExpected) menuID= menus[i].ID;
+            }
+
+            int menuIDTest = menuID;
+            string nameTest = "Phá lấu bò gà heo";
+            int priceTest = 30000;
+            int priceAfterTest = 25000;
+            string rootPath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            byte[] imageTest = File.ReadAllBytes(rootPath + "/data/images/com-chien-ca-man.jpg");
+
+            int menuIDTest1 = menuID;
+            string nameTest1 = "Gà lá giang";
+            int priceTest1 = 39000;
+            int priceAfterTest1 = 25900;
+            string rootPath1 = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            byte[] imageTest1 = File.ReadAllBytes(rootPath + "/data/images/com-chien-ca-man.jpg");
+
+            MenuItem menuItemTest = new MenuItem();
+            menuItemTest.MenuID = menuIDTest;
+            menuItemTest.Name = nameTest;
+            menuItemTest.Price = priceTest;
+            menuItemTest.PriceAfter = priceAfterTest;
+            menuItemTest.Image = imageTest;
+
+            MenuItem menuItemTest1 = new MenuItem();
+            menuItemTest1.MenuID = menuIDTest1;
+            menuItemTest1.Name = nameTest1;
+            menuItemTest1.Price = priceTest1;
+            menuItemTest1.PriceAfter = priceAfterTest1;
+            menuItemTest1.Image = imageTest1;
+
+            MenuItem menuItemExpected = menuItemBLL.CreateMenuItem(menuItemTest);
+            MenuItem menuItemExpected1 = menuItemBLL.CreateMenuItem(menuItemTest1);
+
+            bool isDelete = true;
+            menuBLL.Delete(menu);
+            List<Menu> menus1 = menuBLL.ListMenu();
+            for (int i = 0; i < menus1.Count; i++)
+            {
+                if (menus1[i].ID == menu.ID) isDelete = false;
+            }
+            Assert.AreEqual(isDelete, true);
+        }
     }
 }
